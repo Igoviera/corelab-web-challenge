ea2378ac1273dba7a68a725cc7c7d1d7379784ef
Igo vieira <igo.vieira@live.com>
2022-07-10 17:08:27 -0300

    Finalizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.module.scss';
-import VehiclesPage from './pages/Vehicles';
-import reportWebVitals from './reportWebVitals';
+import './global.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Adicionar from './pages/Adicionar/Adicionar';
+import Filtro from './pages/Filtro/Filtro';
+import Editar from './pages/Editar/Editar';
+import { CartProvider } from './contexts/CartContext';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <VehiclesPage />
-  </React.StrictMode>
+  <CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path ='/' element={<Home/>}/>
+        <Route path='/adicionar' element={<Adicionar/>}/>
+        <Route path='/filtro' element={<Filtro/>}/>
+        <Route path='/editar/:id' element={<Editar/>}/>
+      </Routes>
+    </BrowserRouter>
+  </CartProvider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
